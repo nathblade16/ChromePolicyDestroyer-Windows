7e36efb05923827879dfa131aa67bc1c540b191b
giver of content <110511216+nathblade16@users.noreply.github.com>
2024-06-12 13:03:00 +1000

    Update Destroy.py

--- Destroy.py
+++ Destroy.py
@@ -1,8 +1,13 @@
+import winreg as wrg
 print("chrome policy destroyer for windows")
 print("WARNING, THIS SOFTWARE IS BOTH UNTESTED AND COMES WITH NO WARRENTY, WE TAKE NO RESPONSIBILITY FOR ANYTHING BAD HAPPENING AS A RESULT OF USING THIS SOFTWARE")
 choice = input("Would you still like to run it?")
+def set_reg(name, value):
+  location = wrg.HKEY_LOCAL_MACHINE
+  chrome = wrg.OpenKey(location, "Software\\Policies\\Google\\Chrome\\ExtensionInstallAllowlist") 
+  wrg.SetValueEx(chrome, name, 0, wrg.REG_SZ, value) 
 def destroy():
-  print("this would do the thing but it is not coded yet")
-if(input[0].lower() = "y"):
+  print("work in progress")
+if(choice[0].lower() = "y"):
   destroy()
   
