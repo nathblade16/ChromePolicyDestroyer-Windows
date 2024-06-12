0493a0a1f8979cbaaa20161997b036a08ae6e0f8
giver of content <110511216+nathblade16@users.noreply.github.com>
2024-06-12 12:52:31 +1000

    Create Destroy.py

---
+++ Destroy.py
@@ -0,0 +1,8 @@
+print("chrome policy destroyer for windows")
+print("WARNING, THIS SOFTWARE IS BOTH UNTESTED AND COMES WITH NO WARRENTY, WE TAKE NO RESPONSIBILITY FOR ANYTHING BAD HAPPENING AS A RESULT OF USING THIS SOFTWARE")
+choice = input("Would you still like to run it?")
+def destroy():
+  print("this would do the thing but it is not coded yet")
+if(input[0].lower() = "y"):
+  destroy()
+  
